c3b6e26831488e54f0c0f56420947e85f3ab2f8f
Dan Johnson <daniel.johnson@evanta.com>
2014-06-24 14:12:15 -0700

    initial commit
    Signed-off-by: Dan Johnson <daniel.johnson@evanta.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReadingList::Application.config.secret_key_base = '803645c39125c4be923c6e1d3de7f714980bb631d1512b94237f49b4d19f00ed967a0dba9f3870c71b37d305837d45a343e3aa8639bf3f0c81f421257d0f29f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReadingList::Application.config.session_store :cookie_store, key: '_reading_list_session'
