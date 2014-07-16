e92c84f6acffc22dd5cb577a33d68e3b681b6882
Dan Johnson <daniel.johnson@evanta.com>
2014-07-16 11:59:42 -0700

    fixes bootstrap style being overridden by active-a

---
+++ app/admin/dashboard.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register_page "Dashboard" do
+
+  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+
+  content title: proc{ I18n.t("active_admin.dashboard") } do
+    div class: "blank_slate_container", id: "dashboard_default_message" do
+      span class: "blank_slate" do
+        span I18n.t("active_admin.dashboard_welcome.welcome")
+        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      end
+    end
+
+    panel "Top Rated Books" do
+      # para "Welcome to ActiveAdmin."
+      table do
+        Book.good(10).map do |book|
+          tr
+          td book.title
+          td book.author
+          td book.description
+        end
+      end
+    end
+  end 
+end
