c7587b10dd9fd8a26e6112c45e601fc3fcb95b4b
Dan Johnson <daniel.johnson@evanta.com>
2014-07-16 16:01:42 -0700

    adds enviroment check to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@ Book.destroy_all
 Genre.destroy_all
 BookClub.destroy_all
 
-puts "Reseting auto increment".green
-ActiveRecord::Base.connection.tables.each do |table|
-  ActiveRecord::Base.connection.execute("ALTER TABLE #{table} AUTO_INCREMENT = 1")
+if Rails.env != "production"
+  puts "Reseting auto increment".green
+  ActiveRecord::Base.connection.tables.each do |table|
+    ActiveRecord::Base.connection.execute("ALTER TABLE #{table} AUTO_INCREMENT = 1")
+  end
 end
 
 fiction = Genre.create!(name: 'Fiction')
